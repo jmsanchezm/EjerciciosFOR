63389f2972055332806d3bcc182da4461c8d8566
jmmoreno <jmmoreno@106-05.info2>
2022-11-03 14:43:43 +0100

    EJERCICIO FOR

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
+		
 		int numUs;
 		Scanner read= new Scanner (System.in);
 		System.out.println("Introduzca un número:");
@@ -12,6 +13,7 @@ public class Ejercicio4 {
 		for (int num=3;num<=numUs; num+=3 ) {
 			System.out.println(num);
 		}
+		read.close();
 	}
 
 }
